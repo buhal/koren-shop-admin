2f8c748615969a44bb2527ef024f96f8295eb0b6
Delyan Ninov <dninov@gmail.com>
2026-06-28 08:42:49 +0300

    Revert "Reapply "init""
    This reverts commit 56389a7d917bec82d4c501c82e1efc7be921f621.

---
+++ src/app/shared/components/footer/footer.ts
@@ -0,0 +1,18 @@
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+
+import { Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+
+import { IValues } from '../../interface/setting.interface';
+import { SettingState } from '../../store/state/setting.state';
+
+@Component({
+  selector: 'app-footer',
+  imports: [AsyncPipe],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss',
+})
+export class Footer {
+  setting$: Observable<IValues> = inject(Store).select(SettingState.setting) as Observable<IValues>;
+}
---
+++ src/app/shared/components/ui/form-fields/form-fields.ts
@@ -0,0 +1,19 @@
+import { NgClass } from '@angular/common';
+import { Component, input } from '@angular/core';
+
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-form-fields',
+  imports: [TranslateModule, NgClass],
+  templateUrl: './form-fields.html',
+  styleUrl: './form-fields.scss',
+})
+export class FormFields {
+  readonly class = input<string>('mb-4 row align-items-center g-2');
+  readonly label = input<string>(undefined);
+  readonly labelClass = input<string>('form-label-title col-sm-3 mb-0');
+  readonly gridClass = input<string>('col-sm-9');
+  readonly for = input<string>(undefined);
+  readonly required = input<Boolean>(undefined);
+}
